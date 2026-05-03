fe6826a04c373230c9582e38b8bd45568767b5e4
omercanv1 <24480344@stu.omu.edu.tr>
2026-05-04 01:24:28 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ const VALID_PASSWORD = '123';
 
 app.use(express.json({ limit: '50mb' })); // For gallery uploads
 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.url}`);
-    next();
-});
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
